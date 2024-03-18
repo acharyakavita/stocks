9531bad54fcd176eb1be988c9c29f218b005c883
Kavitha <acharyakavita12@gmail.com>
2024-03-18 12:31:22 -0400

    stockdata

--- source/Components/StockDataScreen.tsx
+++ source/Components/StockDataScreen.tsx
@@ -1,41 +1,50 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import { REACT_APP_API_KEY } from '@env'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { CartesianChart, Line,useChartPressState } from "victory-native";
+import type { SharedValue } from "react-native-reanimated";
 
-const Stack = createNativeStackNavigator()
+const DATA = Array.from({ length: 31 }, (_, i) => ({
+  day: i,
+  highTmp: 40 + 30 * Math.random(),
+}));
 
-export default function StockDataScreen({ navigation, route }) {
-  const getMovies = () => {
-    const output = fetch(
-      `https://finnhub.io/api/v1/search?q=apple&token=${REACT_APP_API_KEY}`,
-      //const output = fetch(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${REACT_APP_API_KEY}`,
-      {
-        method: 'GET',
-      }
-    )
-      .then((resp) => resp.json())
-      .then(function (data) {
-        //console.log(data.map(item=>item.displaySymbol),'\n',)
-        console.log(data.result.map((item) => item.symbol))
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-    return output
-  }
-  //getMovies()
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+  return <Circle cx={x} cy={y} r={8} color="black" />;
+}
+
+export default function StockDataScreen() {
+  const { state, isActive } = useChartPressState({ x: 0, y: { highTmp: 0 } });
   return (
-    <View>
-      <Text>this screen will have {route.params.name} data</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+    <CartesianChart data={DATA} 
+    xKey="x"
+     yKeys={["highTmp"]}  
+    chartPressState={state} 
+    axisOptions={{ font }}>
+      {({ points }) => (
+        <>
+        <Line
+          points={points.highTmp}
+          color="red"
+          strokeWidth={3}
+          animate={{ type: "timing", duration: 300 }}
+        />
+        
+        {isActive ? (
+          <ToolTip x={state.x.position} y={state.y.highTmp.position} />
+        ) : null}
+          </>
+      )}
+    </CartesianChart>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: 300,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
